b4c4077b97e14065d85bd1c151de792df9979dd7
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2022-10-28 14:28:20 +0300

    category screen right recyclerview design and adapter

---
+++ app/src/main/java/com/skapps/shoppingapp/adapter/CategoryRightAdapter.kt
@@ -0,0 +1,27 @@
+package com.skapps.shoppingapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.skapps.shoppingapp.databinding.RowCategoryRightBinding
+import com.skapps.shoppingapp.model.Category
+
+class CategoryRightAdapter(private var categorylist:ArrayList<Category>):RecyclerView.Adapter<CategoryRightAdapter.RightViewHolder>(){
+    class RightViewHolder(private val binding:RowCategoryRightBinding):RecyclerView.ViewHolder(binding.root){
+            fun bind(category: Category){
+                binding.textCategoryRight.text=category.category
+            }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RightViewHolder {
+        val rcvright=RowCategoryRightBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return RightViewHolder(rcvright)
+    }
+
+    override fun onBindViewHolder(holder: RightViewHolder, position: Int) {
+        holder.bind(categorylist.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return categorylist.size
+    }
+}
--- app/src/main/java/com/skapps/shoppingapp/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/skapps/shoppingapp/ui/category/CategoryFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.skapps.shoppingapp.R
 import com.skapps.shoppingapp.adapter.CategoryLeftAdapter
+import com.skapps.shoppingapp.adapter.CategoryRightAdapter
 import com.skapps.shoppingapp.databinding.FragmentCategoryBinding
 import com.skapps.shoppingapp.model.Category
 
@@ -16,6 +19,7 @@ class CategoryFragment : Fragment() {
     private lateinit var viewModel: CategoryViewModel
     private lateinit var binding:FragmentCategoryBinding
     private lateinit var categoryLeftAdapter:CategoryLeftAdapter
+    private lateinit var categoryRightAdapter: CategoryRightAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,12 +37,26 @@ class CategoryFragment : Fragment() {
         clist.add(category)
         clist.add(category)
         clist.add(category)
+        clist.add(category)
+        clist.add(category)
+        clist.add(category)
         viewModel = ViewModelProvider(this).get(CategoryViewModel::class.java)
         binding.leftRcvCategory.apply {
             categoryLeftAdapter= CategoryLeftAdapter(clist)
             adapter=categoryLeftAdapter
             layoutManager=LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
             setHasFixedSize(true)
+        }
+        binding.rightRcvCategory.apply {
+            categoryRightAdapter= CategoryRightAdapter(clist)
+            adapter=categoryRightAdapter
+            layoutManager=GridLayoutManager(requireContext(),2)
+            setHasFixedSize(true)
+        }
+        binding.editTextCategory.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus){
+                findNavController().navigate(R.id.action_categoryFragment_to_searchFragment)
+            }
         }
     }
 
