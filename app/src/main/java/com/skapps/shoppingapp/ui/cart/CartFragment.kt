975156270fec0491ee895d1c0338a087c7887a5e
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2022-10-28 14:44:51 +0300

    create cart fragment and design

---
+++ app/src/main/java/com/skapps/shoppingapp/ui/cart/CartFragment.kt
@@ -0,0 +1,32 @@
+package com.skapps.shoppingapp.ui.cart
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.skapps.shoppingapp.R
+
+class CartFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CartFragment()
+    }
+
+    private lateinit var viewModel: CartViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CartViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/skapps/shoppingapp/ui/cart/CartViewModel.kt
@@ -0,0 +1,7 @@
+package com.skapps.shoppingapp.ui.cart
+
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
