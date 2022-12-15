2a816a50abdab72e97eb08fda4418efe3d593832
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2022-12-15 23:49:18 +0300

    design wallet dialog fragment

--- app/src/main/java/com/skapps/shoppingapp/ui/wallet/WalletDialogFragment.kt
+++ app/src/main/java/com/skapps/shoppingapp/ui/wallet/WalletDialogFragment.kt
@@ -5,14 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.skapps.shoppingapp.R
 
-class WalletBottomFragment :BottomSheetDialogFragment() {
-
-    companion object {
-        fun newInstance() = WalletBottomFragment()
-    }
+class WalletDialogFragment : DialogFragment() {
 
     private lateinit var viewModel: WalletViewModel
 
@@ -23,10 +20,7 @@ class WalletBottomFragment :BottomSheetDialogFragment() {
         return inflater.inflate(R.layout.fragment_wallet, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(WalletViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
-
 }
