1943f039305b23ec05a77d3e5300d4486b131f4d
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2022-12-16 13:50:02 +0300

    added row product card

--- app/src/main/java/com/skapps/shoppingapp/ui/products/ProductsFragment.kt
+++ app/src/main/java/com/skapps/shoppingapp/ui/products/ProductsFragment.kt
@@ -9,10 +9,12 @@ import android.view.ViewGroup
 import com.skapps.shoppingapp.R
 import com.skapps.shoppingapp.databinding.FragmentProductFeaturesBinding
 import com.skapps.shoppingapp.databinding.FragmentProductsBinding
+import com.skapps.shoppingapp.ui.products.productsAdapter.ProductsAdapter
 
 class ProductsFragment : Fragment() {
     private lateinit var viewModel: ProductsViewModel
     private lateinit var binding:FragmentProductsBinding
+    private lateinit var adapter:ProductsAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -24,7 +26,16 @@ class ProductsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProductsViewModel::class.java)
-
     }
 
+    private fun observeLiveData(){
+        viewModel.products.observe(viewLifecycleOwner){ category ->
+            binding.toolbarTitleProducts.text=category.categoryName
+            adapter= ProductsAdapter(category.products){ product, click ->
+
+            }
+            binding.productsRcv.adapter=adapter
+
+        }
+    }
 }
